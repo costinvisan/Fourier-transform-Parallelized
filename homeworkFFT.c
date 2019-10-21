61d8366ad0af67a03450ecbaca47086da665e228
costinvisan <visan.costin47@gmail.com>
2019-10-21 23:52:01 +0300

    update

--- homeworkFFT.c
+++ homeworkFFT.c
@@ -1,30 +1,73 @@
 #include <stdio.h>
 #include <math.h>
 #include <complex.h>
- 
-double PI;
+#include <stdlib.h>
+#include <pthread.h>
+
+struct args {
+	int step;
+	cplx buf[];
+	cplx out[];
+};
+
+int n;
+int P;
+
 typedef double complex cplx;
- 
-void _fft(cplx buf[], cplx out[], int n, int step)
-{
+
+cplx *arr_input;
+
+void _fft(cplx buf[], cplx out[], int step) {
 	if (step < n) {
-		_fft(out, buf, n, step * 2);
-		_fft(out + step, buf + step, n, step * 2);
+
+		if (step < P) {
+			pthread_t tid[P];
+			int thread_id[P];
+
+			args arguments;
+			arguments.step = step * 2;
+			arguments.buf = buf;
+			arguments.out = out;
+
+			for(int i = 0; i < 2; i++)
+				thread_id[i] = i;
+
+			for(int i = 0; i < P; i++) {
+				pthread_create(&(tid[i]), NULL, thread_function, &(arguments));
+			}
+
+			for(int i = 0; i < P; i++) {
+				pthread_join(tid[i], NULL);
+			}
+		}
+
+		_fft(out, buf, step * 2);
+		_fft(out + step, buf + step, step * 2);
  
 		for (int i = 0; i < n; i += 2 * step) {
-			cplx t = cexp(-I * PI * i / n) * out[i + step];
+			cplx t = cexp(-I * M_PI * i / n) * out[i + step];
 			buf[i / 2]     = out[i] + t;
 			buf[(i + n)/2] = out[i] - t;
 		}
 	}
 }
- 
-void fft(cplx buf[], int n)
-{
+
+void *thread_function(void *var) {
+	args arguments = *(args *) var;
+
+	int step = arguments.step;
+	cplx buf[] = arguments.buf;
+	cplx out[] = arguments.out;
+
+	_fft(out, buf, n, step * 2);
+	_fft(out + step, buf + step, n, step * 2);
+}
+
+void fft(cplx buf[], int n) {
 	cplx out[n];
 	for (int i = 0; i < n; i++) out[i] = buf[i];
  
-	_fft(buf, out, n, 1);
+	_fft(buf, out, 1);
 }
  
  
@@ -37,14 +80,28 @@ void show(const char * s, cplx buf[]) {
 			printf("(%g, %g) ", creal(buf[i]), cimag(buf[i]));
 }
  
-int main()
-{
-	PI = atan2(1, 1) * 4;
-	cplx buf[] = {1, 1, 1, 1, 0, 0, 0, 0};
+int main(int argc, char *argv[]) {
+	FILE *fd_input = fopen(argv[1], "r");
+	FILE *fd_output = fopen(argv[2], "w");
+	P = atoi(argv[3]);
+
+	fscanf(fd_input, "%d", &n);
+	
+	arr_input = (cplx *) calloc(n, sizeof(cplx));
+	for (int i = 0; i < n; ++i) {
+		fscanf(fd_input, "%lf", &arr_input[i]);
+	}	
  
-	show("Data: ", buf);
-	fft(buf, 8);
-	show("\nFFT : ", buf);
- 
+ 	fft(arr_input, n);
+
+	fprintf(fd_output, "%d\n", n);
+	for (int i = 0; i < n; ++i) {
+		fprintf(fd_output, "%lf %lf\n", creal(arr_input[i]), cimag(arr_input[i]));
+	}
+
+	free(arr_input);
+	fclose(fd_input);
+	fclose(fd_output);
+
 	return 0;
 }
